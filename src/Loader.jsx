717946b854b37f0368eb32c3ca440163569acc7b
Ramanujam Gond <ramanujamgond@gmail.com>
2023-12-22 19:53:34 +0530

    feat: develop an simaple app in react to search movie name using OMDb API

---
+++ src/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <>
+            <div style={{ display: "flex", alignItems: 'center', justifyContent: "center", marginTop: "25%" }}>
+                <span class="loader"></span>
+            </div>
+        </>
+    )
+}
+
+export default Loader
